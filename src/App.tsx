47b9110b996279cfec77f5bb99c24d1dbff1e056
Ricardo Santos Waldrich <ricardow200510@gmail.com>
2026-03-09 11:12:51 -0300

    09/03 Variantes de texto

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,12 @@
+import Text1 from "./components/text";
+
 export default function App() {
-return <div className= 'text-green-base'>ola mundo</div>;
-  
+return (
+<div className="flex flex-col gap-2">
+    <Text1 variant="body-sm-bold" className="text-pink-base">ola mundo</Text1>
+    <Text1 className="text-green-base">ola mundo</Text1>
+    <Text1 variant="body-sm-bold" >ola mundo</Text1>
+    <Text1 variant="body-sm-bold" >Levar o dog para passear</Text1>
+</div>
+)
 }
